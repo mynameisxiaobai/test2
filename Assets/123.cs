6c717671dcd3ca55c7b8c8867cb4f4e57a4a4c87
leiyi <lyemail@126.com>
2021-06-21 14:49:14 +0800

    123

--- Assets/123.cs
+++ Assets/123.cs
@@ -7,7 +7,7 @@ public class 123 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       123 
     }
 
     // Update is called once per frame
